f71805551f3688ae0ec62df573726cafbc7513a7
Yingqiang Qi <92144386+qyqiang@users.noreply.github.com>
2025-07-21 16:05:24 +0800

    feat: update ui

---
+++ es/components/select/src/option2.mjs
@@ -0,0 +1,117 @@
+import { defineComponent, computed, unref, reactive, toRefs, getCurrentInstance, onBeforeUnmount, nextTick, resolveComponent, withDirectives, openBlock, createElementBlock, normalizeClass, withModifiers, renderSlot, createElementVNode, toDisplayString, createVNode, withCtx, vShow } from 'vue';
+import { useOption } from './useOption.mjs';
+import { COMPONENT_NAME, optionProps } from './option.mjs';
+import _export_sfc from '../../../_virtual/plugin-vue_export-helper.mjs';
+import { useNamespace } from '../../../hooks/use-namespace/index.mjs';
+import { useId } from '../../../hooks/use-id/index.mjs';
+
+const _sfc_main = defineComponent({
+  name: COMPONENT_NAME,
+  componentName: COMPONENT_NAME,
+  props: optionProps,
+  setup(props) {
+    const ns = useNamespace("select");
+    const id = useId();
+    const containerKls = computed(() => [
+      ns.be("dropdown", "item"),
+      ns.is("disabled", unref(isDisabled)),
+      ns.is("selected", unref(itemSelected)),
+      ns.is("hovering", unref(hover))
+    ]);
+    const states = reactive({
+      index: -1,
+      groupDisabled: false,
+      visible: true,
+      hover: false
+    });
+    const {
+      currentLabel,
+      itemSelected,
+      isDisabled,
+      select,
+      hoverItem,
+      updateOption
+    } = useOption(props, states);
+    const { visible, hover } = toRefs(states);
+    const vm = getCurrentInstance().proxy;
+    select.onOptionCreate(vm);
+    onBeforeUnmount(() => {
+      const key = vm.value;
+      nextTick(() => {
+        const { selected: selectedOptions } = select.states;
+        const doesSelected = selectedOptions.some((item) => {
+          return item.value === vm.value;
+        });
+        if (select.states.cachedOptions.get(key) === vm && !doesSelected) {
+          select.states.cachedOptions.delete(key);
+        }
+      });
+      select.onOptionDestroy(key, vm);
+    });
+    function selectOptionClick() {
+      if (!isDisabled.value) {
+        select.handleOptionSelect(vm);
+      }
+    }
+    return {
+      ns,
+      id,
+      containerKls,
+      currentLabel,
+      itemSelected,
+      isDisabled,
+      select,
+      visible,
+      hover,
+      states,
+      hoverItem,
+      updateOption,
+      selectOptionClick
+    };
+  }
+});
+function _sfc_render(_ctx, _cache) {
+  const _component_el_icon = resolveComponent("el-icon");
+  return withDirectives((openBlock(), createElementBlock("li", {
+    id: _ctx.id,
+    class: normalizeClass(_ctx.containerKls),
+    role: "option",
+    "aria-disabled": _ctx.isDisabled || void 0,
+    "aria-selected": _ctx.itemSelected,
+    onMousemove: _ctx.hoverItem,
+    onClick: withModifiers(_ctx.selectOptionClick, ["stop"])
+  }, [
+    renderSlot(_ctx.$slots, "default", {}, () => [
+      createElementVNode("div", { class: "option-wrap" }, [
+        createElementVNode("div", { class: "option-wrap-content" }, toDisplayString(_ctx.currentLabel), 1),
+        withDirectives(createElementVNode("div", { class: "option-wrap-icon" }, [
+          createVNode(_component_el_icon, { size: "16px" }, {
+            default: withCtx(() => [
+              (openBlock(), createElementBlock("svg", {
+                xmlns: "http://www.w3.org/2000/svg",
+                width: "16",
+                height: "16",
+                viewBox: "0 0 16 16",
+                fill: "none"
+              }, [
+                createElementVNode("path", {
+                  d: "M5.20006 14.2833C4.97716 14.2834 4.75643 14.2395 4.55052 14.1542C4.3446 14.0688 4.15754 13.9437 4.00006 13.786L0.292725 10.0807L1.70739 8.66665L5.20006 12.1593L14.2927 3.06665L15.7074 4.48065L6.40006 13.786C6.24257 13.9437 6.05552 14.0688 5.8496 14.1542C5.64369 14.2395 5.42296 14.2834 5.20006 14.2833Z",
+                  fill: "#2A3F4D"
+                })
+              ]))
+            ]),
+            _: 1
+          })
+        ], 512), [
+          [vShow, _ctx.itemSelected]
+        ])
+      ])
+    ])
+  ], 42, ["id", "aria-disabled", "aria-selected", "onMousemove", "onClick"])), [
+    [vShow, _ctx.visible]
+  ]);
+}
+var Option = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "option.vue"]]);
+
+export { Option as default };
+//# sourceMappingURL=option2.mjs.map
